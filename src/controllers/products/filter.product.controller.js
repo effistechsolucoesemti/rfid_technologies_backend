04bd323713cf8aaff664a48eb73c8ac95a3bc753
Matheus Loubake <mloubake@hotmail.com>
2022-01-07 20:51:06 -0300

    Spliting SearchInput array to SelectOption and SearchValue.

--- src/controllers/products/filter.product.controller.js
+++ src/controllers/products/filter.product.controller.js
@@ -1,36 +1,33 @@
 import { database } from "../../services/firebase.js";
 import {
   endAt,
-  equalTo,
   get,
-  limitToFirst,
-  onValue,
   orderByChild,
-  orderByKey,
-  orderByValue,
   query,
   ref,
   startAt,
 } from "@firebase/database";
 
 export const filterProduct = (request, response) => {
-  const { user_key, id_token, searchInput } = request.body;
+  console.log("CALLING FILTER PRODUCT METHOD");
 
-  console.log({ reqBody: request.body }, { SI: request.body.searchInput[0] });
+  const { user_key, id_token, selectedOptions, searchValue } = request.body;
+
+  console.log({ reqBody: request.body }, { selectedOptions, searchValue });
 
   try {
     get(
       query(
         ref(database, `users/${user_key}/table_of_products/`),
         ...[
-          orderByChild(searchInput[0]),
-          startAt(searchInput[1]),
-          endAt(searchInput[1] + "\uf8ff"),
+          orderByChild(selectedOptions),
+          startAt(searchValue),
+          endAt(searchValue + "\uf8ff"),
         ]
       )
     )
       .then((data) => {
-        console.log(data.val());
+        console.log({ data: data.val() });
         return response.send(data.val());
       })
       .catch((error) => {
